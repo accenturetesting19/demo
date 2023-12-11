89cfad55336db5bfc12d33d24012f62a0be2f214
accenturetesting19 <149995453+accenturetesting19@users.noreply.github.com>
2023-12-11 12:08:59 +0530

    Update sample.ts

--- sample.ts
+++ sample.ts
@@ -1,11 +1,3 @@
-let message: string = 'Hello, World
-#console.log("Hello");
+#let message: string = 'Hello, World
+
 console.log(message);
-function f(a, g) { // Noncompliant: 'f' returns 'b' on two different return statements
-    const b = 42;
-    if (a) {
-      g(a);
-      return b;
-    }
-    return b;
-  }
