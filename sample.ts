540a065ac33532181a5ac98e7d72a6bc7ac6953d
accenturetesting19 <amsapna19@gmail.com>
2023-12-11 11:34:32 +0530

    Changing file to have single line

--- sample.ts
+++ sample.ts
@@ -1,4 +1,2 @@
-let message: string = 'Hello, World';
-let message: string = 'Hello, World';
-console.log(message);
-var mysql = require('mysql');
+#let message: string = 'Hello, World';
+console.log("Hello");
