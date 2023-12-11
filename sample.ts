0a618fd7a30fd6d9cdf0f562049320c4ce84ab3b
accenturetesting19 <amsapna19@gmail.com>
2023-12-11 11:58:25 +0530

    Added new message with invalid

--- sample.ts
+++ sample.ts
@@ -1,3 +1,11 @@
 let message: string = 'Hello, World
 #console.log("Hello");
 console.log(message);
+function f(a, g) { // Noncompliant: 'f' returns 'b' on two different return statements
+    const b = 42;
+    if (a) {
+      g(a);
+      return b;
+    }
+    return b;
+  }
